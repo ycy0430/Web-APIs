199c248004acac94248805c7677fd41a597032ce
yangchengye123 <1192724658@qq.com>
2020-08-27 16:34:49 +0800

    轮播图

---
+++ 轮播图/js/index.js
@@ -0,0 +1,127 @@
+window.addEventListener('load', function() {
+    var focus = document.querySelector('.focus');
+    var arrow_l = document.querySelector('.arrow-l');
+    var arrow_r = document.querySelector('.arrow-r');
+    focus.addEventListener('selectstart', function(e) {
+        e.preventDefault();
+    })
+    focus.addEventListener('mouseenter', function() {
+        arrow_l.style.display = 'block';
+        arrow_r.style.display = 'block';
+        clearInterval(timer);
+        timer = null;
+    })
+    focus.addEventListener('mouseleave', function() {
+        arrow_l.style.display = 'none';
+        arrow_r.style.display = 'none';
+        timer = setInterval(function() {
+            arrow_r.click();
+        }, 2000);
+    })
+    var ul = focus.querySelector('.ul-img');
+    var ol = focus.querySelector('.ft-nav');
+    var focusWidth = focus.offsetWidth;
+
+    for (var i = 0; i < ul.children.length; i++) {
+        var li = document.createElement('li');
+        li.setAttribute('index', i);
+        ol.appendChild(li);
+        li.addEventListener('click', function() {
+            for (var i = 0; i < ol.children.length; i++) {
+                ol.children[i].className = '';
+            }
+            this.className = 'current';
+            var index = this.getAttribute('index');
+            num = index;
+            circle = index;
+            console.log(focusWidth);
+            console.log(index);
+            animate(ul, -index * focusWidth);
+        })
+    }
+    ol.children[0].className = 'current';
+    var first = ul.children[0].cloneNode(true);
+    ul.appendChild(first);
+    var num = 0;
+    var circle = 0;
+    var flag = true;
+
+    arrow_r.addEventListener('click', function() {
+        if (flag) {
+            flag = false;
+            if (num == ul.children.length - 1) {
+                ul.style.left = 0;
+                num = 0;
+            }
+            num++;
+            animate(ul, -num * focusWidth, function() {
+                flag = true;
+            });
+            circle++;
+            if (circle == ol.children.length) {
+                circle = 0;
+            }
+            circleChange();
+
+        }
+    })
+
+    arrow_l.addEventListener('click', function() {
+        if (flag) {
+            flag = false;
+            if (num == 0) {
+                num = ul.children.length - 1;
+                ul.style.left = -num * focusWidth + 'px';
+
+            }
+            num--;
+            animate(ul, -num * focusWidth, function() {
+                flag = true;
+            });
+            circle--;
+            circle = circle < 0 ? ol.children.length - 1 : circle;
+            circleChange();
+        }
+    });
+
+    function circleChange() {
+        for (var i = 0; i < ol.children.length; i++) {
+            ol.children[i].className = '';
+
+        }
+        ol.children[circle].className = 'current';
+    }
+    var timer = setInterval(function() {
+        arrow_r.click();
+    }, 2000);
+
+
+
+
+
+    function animate(obj, target, callback) {
+        clearInterval(obj.timer);
+        obj.timer = setInterval(function() {
+            var step = (target - obj.offsetLeft) / 10;
+            step = step > 0 ? Math.ceil(step) : Math.floor(step);
+            if (obj.offsetLeft == target) {
+                clearInterval(obj.timer);
+                callback && callback();
+            }
+            obj.style.left = obj.offsetLeft + step + 'px';
+        }, 15);
+        console.log('111');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+})
